e7dcb0b52e51bc88836cd5d730a5782a02366720
Yangsibai <yangsibai@gmail.com>
2016-06-27 11:24:33 +0800

    fix compatibility for IE9,10

--- src/ScrollView.jsx
+++ src/ScrollView.jsx
@@ -1,7 +1,11 @@
 'use strict';
 
-import {Component, PropTypes} from 'react';
-import {findDOMNode} from 'react-dom';
+var React = require('react');
+var PropTypes = React.PropTypes;
+var findDOMNode = require('react-dom').findDOMNode;
+//
+// import {Component, PropTypes} from 'react';
+// import {findDOMNode} from 'react-dom';
 
 const STYLE_AUTO = 'auto';
 const STYLE_SHOW = 'show';
@@ -9,152 +13,156 @@ const STYLE_HIDE = 'hide';
 
 const IS_LION_SCROLLBAR = (/iPhone|iPad|iPod|OS X/i).test(window.navigator.userAgent);
 
-class ScrollView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+
+module.exports = React.createClass({
+  propTypes: {
+    scrollBarStyle: PropTypes.string,
+    defaultScrollTop: PropTypes.number,
+    defaultScrollLeft: PropTypes.number,
+    scrollTop: PropTypes.number,
+    scrollLeft: PropTypes.number,
+    warning: PropTypes.bool,
+    onScroll: PropTypes.func
+  },
+  getInitialState: function () {
+    return {
       loaded: false,
       vSize: 0,
       vPosition: 0,
       hSize: 0,
       hPosition: 0,
       hide: this.props.scrollBarStyle === STYLE_HIDE,
-      alwaysShow: false
+      alwaysShow: false,
+      scrollBarStyle: this.props.scrollBarStyle || 'auto', // auto | show | hide
+      hideTimeout: this.props.hideTimeout || 1500,
+      defaultScrollTop: this.props.defaultScrollTop || 0,
+      defaultScrollLeft: this.props.defaultScrollLeft || 0,
+      scrollTop: this.props.scrollTop || -1,
+      scrollLeft: this.props.scrollLeft || -1,
+      warning: this.props.warning || true
     };
-
-    this.onScroll = () => {
-      this.onUpdate();
-      if (this.props.onScroll) {
-        var node = findDOMNode(this.refs.content);
-        this.props.onScroll(node.scrollTop, node.scrollLeft);
-      }
-    };
-
-    this.onUpdate = ()=> {
+  },
+  onScroll: function () {
+    this.onUpdate();
+    if (this.props.onScroll) {
       var node = findDOMNode(this.refs.content);
-      this.setState({
-        vSize: node.offsetHeight / node.scrollHeight,
-        vPosition: node.scrollTop / node.scrollHeight,
-        hSize: node.offsetWidth / node.scrollWidth,
-        hPosition: node.scrollLeft / node.scrollWidth,
-        loaded: true,
-        hide: this.props.scrollBarStyle === STYLE_HIDE
-      });
-    };
+      this.props.onScroll(node.scrollTop, node.scrollLeft);
+    }
+  },
+  onUpdate: function () {
+    var node = findDOMNode(this.refs.content);
+    this.setState({
+      vSize: node.offsetHeight / node.scrollHeight,
+      vPosition: node.scrollTop / node.scrollHeight,
+      hSize: node.offsetWidth / node.scrollWidth,
+      hPosition: node.scrollLeft / node.scrollWidth,
+      loaded: true,
+      hide: this.state.scrollBarStyle === STYLE_HIDE
+    });
+  },
+  onMouseMove: function (e) {
+    var node = findDOMNode(this.refs.content);
+    var delta, newPosition;
+    if (this.direction === 'vertical') {
+      delta = (e.pageY - this.start) / node.offsetHeight;
+      newPosition = Math.min(Math.max(0, this.startPosition + delta), this.max);
+      node.scrollTop = node.scrollHeight * newPosition;
+    } else {
+      delta = (e.pageX - this.start) / node.offsetWidth;
+      newPosition = Math.min(Math.max(0, this.startPosition + delta), this.max);
+      node.scrollLeft = node.scrollWidth * newPosition;
+    }
+    e.stopPropagation();
+    e.preventDefault();
+  },
+  onMouseUp: function (e) {
+    document.body.removeEventListener('mousemove', onMouseMove);
+    document.body.removeEventListener('mouseup', onMouseUp);
+    this.setState({
+      mouseDown: false
+    });
+    e.stopPropagation();
+    e.preventDefault();
+  },
+  onVerticalThumbMouseDown: function (e) {
+    this.direction = 'vertical';
+    var node = findDOMNode(this.refs.content);
+    this.start = e.pageY;
+    this.startPosition = this.state.vPosition;
+    this.max = (node.scrollHeight - node.offsetHeight) / node.scrollHeight;
+    document.body.addEventListener('mousemove', onMouseMove);
+    document.body.addEventListener('mouseup', onMouseUp);
+    this.setState({
+      mouseDown: true
+    });
+    e.stopPropagation();
+    e.preventDefault();
+  },
+  onHorizontalThumbMouseDown: function () {
+    this.direction = 'horizontal';
+    var node = findDOMNode(this.refs.content);
+    this.start = e.pageX;
+    this.startPosition = this.state.hPosition;
+    this.max = (node.scrollWidth - node.offsetWidth) / node.scrollWidth;
+    document.body.addEventListener('mousemove', onMouseMove);
+    document.body.addEventListener('mouseup', onMouseUp);
+    this.setState({
+      mouseDown: true
+    });
+    e.stopPropagation();
+    e.preventDefault();
+  },
 
-    var start, startPosition, max, direction;
+  setAutoHideTimer: function () {
+    if (this.hideTimer) {
+      clearTimeout(this.hideTimer);
+      this.hideTimer = null;
+    }
 
-    var onMouseMove = (e)=> {
-      var node = findDOMNode(this.refs.content);
-      var delta, newPosition;
-      if (direction === 'vertical') {
-        delta = (e.pageY - start) / node.offsetHeight;
-        newPosition = Math.min(Math.max(0, startPosition + delta), max);
-        node.scrollTop = node.scrollHeight * newPosition;
-      } else {
-        delta = (e.pageX - start) / node.offsetWidth;
-        newPosition = Math.min(Math.max(0, startPosition + delta), max);
-        node.scrollLeft = node.scrollWidth * newPosition;
-      }
-      e.stopPropagation();
-      e.preventDefault();
-    };
+    if (this.state.scrollBarStyle === STYLE_AUTO) {
+      this.hideTimer = setTimeout(()=> {
+        this.setState({
+          hide: true
+        });
+      }, this.state.hideTimeout);
+    }
+  },
 
-    var onMouseUp = (e) => {
-      document.body.removeEventListener('mousemove', onMouseMove);
-      document.body.removeEventListener('mouseup', onMouseUp);
-      this.setState({
-        mouseDown: false
-      });
-      e.stopPropagation();
-      e.preventDefault();
-    };
-
-    this.onVerticalThumbMouseDown = (e)=> {
-      direction = 'vertical';
-      var node = findDOMNode(this.refs.content);
-      start = e.pageY;
-      startPosition = this.state.vPosition;
-      max = (node.scrollHeight - node.offsetHeight) / node.scrollHeight;
-      document.body.addEventListener('mousemove', onMouseMove);
-      document.body.addEventListener('mouseup', onMouseUp);
-      this.setState({
-        mouseDown: true
-      });
-      e.stopPropagation();
-      e.preventDefault();
-    };
-
-    this.onHorizontalThumbMouseDown = (e)=> {
-      direction = 'horizontal';
-      var node = findDOMNode(this.refs.content);
-      start = e.pageX;
-      startPosition = this.state.hPosition;
-      max = (node.scrollWidth - node.offsetWidth) / node.scrollWidth;
-      document.body.addEventListener('mousemove', onMouseMove);
-      document.body.addEventListener('mouseup', onMouseUp);
-      this.setState({
-        mouseDown: true
-      });
-      e.stopPropagation();
-      e.preventDefault();
-    };
-
-    this.hideTimer = null;
-
-    this.setAutoHideTimer = () => {
-      if (this.hideTimer) {
-        clearTimeout(this.hideTimer);
-        this.hideTimer = null;
-      }
-
-      if (this.props.scrollBarStyle === STYLE_AUTO) {
-        this.hideTimer = setTimeout(()=> {
-          this.setState({
-            hide: true
-          });
-        }, this.props.hideTimeout);
-      }
-    };
-
-    this.onScrollBarMouseEnter = (e) => {
-      this.setState({
-        mouseEntered: true
-      });
-    };
-
-    this.onScrollBarMouseOut = (e) => {
-      this.setState({
-        mouseEntered: false
-      });
-    };
-  }
-
-  componentDidMount() {
+  onScrollBarMouseEnter: function (e) {
+    this.setState({
+      mouseEntered: true
+    });
+  },
+  onScrollBarMouseOut: function (e) {
+    this.setState({
+      mouseEntered: false
+    });
+  },
+  componentDidMount: function () {
     this.onUpdate();
     var node = findDOMNode(this.refs.content);
-    var props = this.props;
-    if (props.defaultScrollTop > 0) {
-      node.scrollTop = props.defaultScrollTop;
+    var state = this.state;
+    if (state.defaultScrollTop > 0) {
+      node.scrollTop = state.defaultScrollTop;
     }
-    if (props.defaultScrollLeft > 0) {
-      node.scrollLeft = props.defaultScrollLeft;
+    if (state.defaultScrollLeft > 0) {
+      node.scrollLeft = state.defaultScrollLeft;
     }
-    if (props.scrollTop > 0) {
-      node.scrollTop = props.scrollTop;
+    if (state.scrollTop > 0) {
+      node.scrollTop = state.scrollTop;
     }
-    if (props.scrollLeft > 0) {
-      node.scrollLeft = props.scrollLeft;
+    if (state.scrollLeft > 0) {
+      node.scrollLeft = state.scrollLeft;
     }
-    if (props.scrollTop >= 0 || props.scrollLeft >= 0 && !props.warning) {
+    if (state.scrollTop >= 0 || state.scrollLeft >= 0 && !state.warning) {
       console.warn(`\
 WARNING: After setting scrollTop/scrollLeft, component will use these values in every updating.
 If you want to set initial scrollTop/scrollLeft, please set defaultScrollLeft/defaultScrollTop.
 You can set property warning = false to hide this warning.`);
     }
-  }
+  },
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps: function (nextProps) {
     var node = findDOMNode(this.refs.content);
     if (nextProps.scrollTop >= 0) {
       node.scrollTop = nextProps.scrollTop;
@@ -162,17 +170,16 @@ You can set property warning = false to hide this warning.`);
     if (nextProps.scrollLeft >= 0) {
       node.scrollLeft = nextProps.scrollLeft;
     }
-  }
+  },
 
-  componentDidUpdate() {
+  componentDidUpdate: function () {
     this.setAutoHideTimer();
-  }
+  },
 
-  componentWillUnmount() {
+  componentWillUnmount: function () {
     clearTimeout(this.hideTimer);
-  }
-
-  render() {
+  },
+  render: function () {
     if (IS_LION_SCROLLBAR) {
       return (
         <div ref="content" style={{
@@ -193,7 +200,7 @@ You can set property warning = false to hide this warning.`);
                onMouseDown={this.onHorizontalThumbMouseDown}
                onMouseEnter={this.onScrollBarMouseEnter}
                onMouseLeave={this.onScrollBarMouseOut}>
-            <div className="thumb" style={this.horizontalThumbStyle}></div>
+            <div className="thumb" style={this.horizontalThumbStyle()}></div>
           </div>
         }
         {
@@ -202,22 +209,21 @@ You can set property warning = false to hide this warning.`);
                onMouseDown={this.onVerticalThumbMouseDown}
                onMouseEnter={this.onScrollBarMouseEnter}
                onMouseLeave={this.onScrollBarMouseOut}>
-            <div className="thumb" style={this.verticalThumbStyle}></div>
+            <div className="thumb" style={this.verticalThumbStyle()}></div>
           </div>
         }
       </div>
     );
-  }
+  },
+  shouldShowScrollBar: function () {
+    return this.state.scrollBarStyle !== STYLE_HIDE;
+  },
 
-  get shouldShowScrollBar() {
-    return this.props.scrollBarStyle !== STYLE_HIDE;
-  }
-
-  get shouldScrollBarAlwaysShow() {
+  shouldScrollBarAlwaysShow: function () {
     return this.state.mouseEntered || this.state.mouseDown;
-  }
+  },
 
-  get verticalThumbStyle() {
+  verticalThumbStyle: function () {
     return {
       height: this.state.vSize * 100 + '%',
       top: this.state.vPosition * 100 + '%',
@@ -225,9 +231,9 @@ You can set property warning = false to hide this warning.`);
       && (this.shouldScrollBarAlwaysShow || (!this.state.hide && this.state.loaded))
         ? 'block' : 'none'
     };
-  }
+  },
 
-  get horizontalThumbStyle() {
+  horizontalThumbStyle: function () {
     return {
       width: this.state.hSize * 100 + '%',
       left: this.state.hPosition * 100 + '%',
@@ -236,26 +242,4 @@ You can set property warning = false to hide this warning.`);
         ? 'block' : 'none'
     };
   }
-}
-
-ScrollView.defaultProps = {
-  scrollBarStyle: 'auto', // auto | show | hide
-  hideTimeout: 1500,
-  defaultScrollTop: 0,
-  defaultScrollLeft: 0,
-  scrollTop: -1,
-  scrollLeft: -1,
-  warning: true
-};
-
-ScrollView.propTypes = {
-  scrollBarStyle: PropTypes.string,
-  defaultScrollTop: PropTypes.number,
-  defaultScrollLeft: PropTypes.number,
-  scrollTop: PropTypes.number,
-  scrollLeft: PropTypes.number,
-  warning: PropTypes.bool,
-  onScroll: PropTypes.func
-};
-
-export default ScrollView;
+});
